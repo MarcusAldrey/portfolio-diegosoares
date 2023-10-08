c7b8c1181fae27cf74ae400fe5ef8ccb785c3800
Marcus Aldrey <marcusaldrey@gmail.com>
2023-10-08 18:50:02 -0300

    initial commit

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Diego Soares - Portfolio</title>
+        <meta name="description" content="Diego Soares - Diretor de arte - Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    <main>
+      <div >
+      Diego Soares
+      </div>
+      
+    </main>
+    </div>
+  )
+}
